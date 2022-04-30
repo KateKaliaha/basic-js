b1cce0ca773f499abf28cf3c7ca541e4851d36b2
Katherina Kaliaha <mellinda077@mail.ru>
2022-04-30 12:35:29 +0300

    complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+ class ValidationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ValidationError";
+  }
 }
+
+
+
+ function transform(arr) {
+  let result = [];
+  if (!Array.isArray(arr)) {
+    throw new ValidationError(`'arr' parameter must be an instance of the Array!`);
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '--discard-next' || arr[i] === '--discard-prev' || arr[i] === '--double-next' || arr[i] === '--double-prev') {
+        switch(arr[i]) {
+          case '--discard-next':
+            if( typeof arr[i + 1] == 'number') {
+              i +=2;
+            }
+            break;
+    
+            case '--discard-prev':
+              if( typeof arr[i - 1] == 'number') {
+                let index = result.indexOf(arr[i - 1])
+                result.splice(index, 1)
+              }
+          break;
+          
+              case '--double-next':
+                if( typeof arr[i + 1] == 'number') {
+                  result.push(arr[i + 1]);
+                }
+            break;
+    
+          case '--double-prev':
+            if( typeof arr[i - 1] == 'number') {
+            result.push(result[result.length - 1]);
+            }
+            break;
+           
+        }
+        } else {
+          result.push(arr[i])
+        }
+      }
+   
+  }
+  return (result);
+}
+
 
 module.exports = {
   transform
